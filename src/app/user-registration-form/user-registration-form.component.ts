37cefc175522e9c96a1a06852985481439d88a3f
akr0051 <akr0051@yahoo.com>
2021-08-18 13:37:04 -0500

    added login

---
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  styleUrls: ['./user-registration-form.component.scss']
+})
+export class UserRegistrationFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+ngOnInit(): void {
+}
+
+registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
+      localStorage.setItem('username', response.user.username);
+      localStorage.setItem('token', response.token);
+
+     this.dialogRef.close(); 
+     this.snackBar.open('User registered', 'OK', {
+        duration: 2000
+     });
+    }, (response) => {
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+  }
